36f666d457dae60cc695269795d7255ecae1e5cc
bemds1 <bemds1@aluno.ifnmg.edu.br>
2024-05-19 16:36:45 -0300

    first commit

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,27 @@
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        // Recebe os dados 
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Conta:");
+        int numero = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Por favor, digite o nome do Cliente:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo:");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + 
+        ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + 
+        ", conta " + numero + 
+        " e seu saldo " + saldo + 
+        " já está disponível para saque.");
+
+        scanner.close();
+    }
+}
